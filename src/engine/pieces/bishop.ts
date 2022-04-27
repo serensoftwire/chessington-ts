93a34845a4d060995b570f3512c2560073e88af3
Seren Kwok <seren.kwok@softwire.com>
2022-04-27 12:07:49 +0100

    Implement bishop diagonal movement

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Board from "../board";
 import Player from "../player";
+import Square from "../square";
+import GameSettings from "../gameSettings";
 
 export default class Bishop extends Piece {
     constructor(player: Player) {
@@ -8,6 +10,36 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board: Board) {
-        return new Array(0);
+        const moves = [];
+        const currentRow = this.getCurrentRow(board);
+        const currentCol = this.getCurrentCol(board);
+
+        const invStart = currentRow + currentCol;
+        const intercept = Math.max(currentRow, currentCol) - Math.min(currentRow, currentCol);
+        let zeroStart = 0;
+
+        for (let row = invStart; row >= 0; row--) {
+            if (row != currentRow) {
+                moves.push(Square.at(row, invStart - row));
+            }
+        }
+
+        if (currentRow < currentCol) {
+            for (let col = intercept; col < GameSettings.BOARD_SIZE; col++) {
+                if (col != currentCol) {
+                    moves.push(Square.at(zeroStart, col));
+                }
+                zeroStart++;
+            }
+        } else {
+            for (let row = intercept; row < GameSettings.BOARD_SIZE; row++) {
+                if (row != currentRow) {
+                    moves.push(Square.at(row, zeroStart));
+                }
+                zeroStart++;
+            }
+        }
+
+        return moves;
     }
 }
