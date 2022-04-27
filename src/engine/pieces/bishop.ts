0256b29806e45041294f9deae6d9cfb6104832af
Seren Kwok <seren.kwok@softwire.com>
2022-04-27 12:37:01 +0100

    Separate functionality of getAvailableMoves

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -9,36 +9,52 @@ export default class Bishop extends Piece {
         super(player);
     }
 
-    getAvailableMoves(board: Board) {
-        const moves = [];
-        const currentRow = this.getCurrentRow(board);
-        const currentCol = this.getCurrentCol(board);
+    private static generateDownDiagonalMoves = (currentCol: number, currentRow: number) => {
+        const downDiagonalMoves: Square[] = [];
+        const invStart: number = currentRow + currentCol;
 
-        const invStart = currentRow + currentCol;
-        const intercept = Math.max(currentRow, currentCol) - Math.min(currentRow, currentCol);
-        let zeroStart = 0;
-
-        for (let row = invStart; row >= 0; row--) {
+        for (let row: number = invStart; row >= 0; row--) {
             if (row != currentRow) {
-                moves.push(Square.at(row, invStart - row));
+                downDiagonalMoves.push(Square.at(row, invStart - row));
             }
         }
 
-        if (currentRow < currentCol) {
-            for (let col = intercept; col < GameSettings.BOARD_SIZE; col++) {
-                if (col != currentCol) {
-                    moves.push(Square.at(zeroStart, col));
-                }
-                zeroStart++;
+        return downDiagonalMoves;
+    }
+
+    private static createUpDiagonalMove = (zeroStart: number, higherCoordinate: number, higherIsCol: boolean) => {
+        return higherIsCol ? Square.at(zeroStart, higherCoordinate) : Square.at(higherCoordinate, zeroStart);
+    }
+
+    private static getUpDiagonalMoves = (intercept: number, currentHigher: number, higherIsCol: boolean) => {
+        const upDiagonalMoves: Square[] = [];
+        let zeroStart: number = 0;
+
+        for (let higherCoordinate: number = intercept; higherCoordinate < GameSettings.BOARD_SIZE; higherCoordinate++) {
+            if (higherCoordinate != currentHigher) {
+                upDiagonalMoves.push(Bishop.createUpDiagonalMove(zeroStart, higherCoordinate, higherIsCol));
             }
-        } else {
-            for (let row = intercept; row < GameSettings.BOARD_SIZE; row++) {
-                if (row != currentRow) {
-                    moves.push(Square.at(row, zeroStart));
-                }
-                zeroStart++;
-            }
+            zeroStart++;
         }
+
+        return upDiagonalMoves;
+    }
+
+    private static generateUpDiagonalMoves = (currentCol: number, currentRow: number) => {
+        const higherCoordinate: number = Math.max(currentCol, currentRow);
+        const intercept: number = higherCoordinate - Math.min(currentRow, currentCol);
+        const higherIsCol: boolean = currentCol > currentRow
+
+        return Bishop.getUpDiagonalMoves(intercept, higherCoordinate, higherIsCol);
+    }
+
+    getAvailableMoves(board: Board) {
+        const moves: Square[] = [];
+        const currentRow: number = this.getCurrentRow(board);
+        const currentCol: number = this.getCurrentCol(board);
+
+        moves.push(...Bishop.generateDownDiagonalMoves(currentCol, currentRow));
+        moves.push(...Bishop.generateUpDiagonalMoves(currentCol, currentRow));
 
         return moves;
     }
